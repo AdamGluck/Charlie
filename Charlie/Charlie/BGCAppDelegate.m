49cba796aba5d6abed548151cc0bd87b07017380
AdamGluck <adamt.gluck@gmail.com>
2013-07-13 16:45:03 -0500

    attempt at text-to-speech

--- Charlie/Charlie/BGCAppDelegate.m
+++ Charlie/Charlie/BGCAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "BGCAppDelegate.h"
+#import "ATTSpeechKit.h"
 
 @implementation BGCAppDelegate
 
@@ -39,6 +40,7 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
--- Charlie/Charlie/BGCMapViewController.m
+++ Charlie/Charlie/BGCMapViewController.m
@@ -12,8 +12,14 @@
 #import "GoogleRoute.h"
 #import "BGCTurnByTurnInstructions.h"
 #import "BGCCrimeDataAccess.h"
+#import <AVFoundation/AVAudioPlayer.h>
+#import <AVFoundation/AVAudioSession.h>
+#import "SpeechConfig.h"
+#import "SpeechAuth.h"
+#import "TTSRequest.h"
+#import "ATTSpeechKit.h"
 
-@interface BGCMapViewController () <CLLocationManagerDelegate, GoogleRouteDelegate, BGCCrimeDataAccessDelegate> {
+@interface BGCMapViewController () <CLLocationManagerDelegate, GoogleRouteDelegate, BGCCrimeDataAccessDelegate, ATTSpeechServiceDelegate> {
     GMSMapView * mapView_;
     BOOL routing;
     BOOL shouldShowAlert;
@@ -21,6 +27,10 @@
 @property (strong, nonatomic) CLLocationManager * locationManager;
 @property (strong, nonatomic) BGCTurnByTurnInstructions * instructions;
 @property (strong, nonatomic) NSMutableArray * heatCircles;
+@property (strong, nonatomic) NSString * ttsInProgress;
+@property (strong, nonatomic) AVAudioPlayer * audioPlayer;
+@property (strong, nonatomic) NSString* oauthToken;
+
 
 @end
 
@@ -41,6 +51,7 @@
     [self configureNavBar];
     [self configureMapView];
     [self fillData];
+    //[self readyForSpeech];
     
 }
 
@@ -315,6 +326,232 @@
     
     return _heatCircles;
 }
+
+/*
+#pragma mark - TTS API
+- (void) prepareSpeech
+{
+    // Set up this application for audio output.
+    NSError* error = nil;
+    [[AVAudioSession sharedInstance] setCategory: AVAudioSessionCategoryPlayback error: &error];
+    if (error != nil) {
+        NSLog(@"Not able to initialize audio session for playback: %@", error);
+    }
+    
+    // Access the SpeechKit singleton.
+    ATTSpeechService* speechService = [ATTSpeechService sharedSpeechService];
+    
+    // Point to the SpeechToText API.
+    speechService.recognitionURL = SpeechServiceUrl();
+    
+    // Hook ourselves up as a delegate so we can get called back with the response.
+    speechService.delegate = self;
+    
+    // Use default speech UI.
+    speechService.showUI = YES;
+    
+    // Choose the speech recognition package.
+    speechService.speechContext = @"QuestionAndAnswer";
+    
+    // Start the OAuth background operation, disabling the Talk button until
+    // it's done.
+    [self validateOAuthForService: speechService];
+    
+    // Wake the audio components so there is minimal delay on the first request.
+    [speechService prepare];
+}
+
+- (void) readyForSpeech
+{
+    // First enable the speech buttons.
+    // Make Text to Speech request that will speak out a greeting.
+    [self startTTS: NSLocalizedString(@"Say Something", @"Greeting")];
+}
+
+
+- (void) startTTS: (NSString*) textToSpeak
+{
+    TTSRequest* tts = [TTSRequest forService: TTSUrl() withOAuth: self.oauthToken];
+    self.ttsInProgress = textToSpeak;
+    [tts postText: textToSpeak forClient: ^(NSData* audioData, NSError* error) {
+        if (![textToSpeak isEqualToString: self.ttsInProgress]) {
+            // TTS was canceled, so don't play it back.
+        }
+        else if (audioData != nil) {
+            NSLog(@"Text to Speech returned %d bytes of audio.", audioData.length);
+            [self playAudioData: audioData];
+        }
+        else {
+            NSLog(@"Unable to convert text to speech: %@", error);
+            // Real applications probably shouldn't display an alert.
+            [self showAlertWithTitle: @"An error occurred"
+                             message: @"Unable to convert text to speech."];
+        }
+    }];
+}
+
+
+- (void) stopTTS
+{
+    // Cancel any TTS request in progress.
+    self.ttsInProgress = nil;
+    // Stop audio playback.
+    [self stopPlaying];
+}
+
+- (void) playAudioData: (NSData*) audioData
+{
+    [self stopPlaying];
+    NSError* error = nil;
+    // Set up this application for audio output.
+    // We have to do this after microphone input, because otherwise the OS
+    // will route audio to the phone receiver, not the speaker.
+    [[AVAudioSession sharedInstance] setCategory: AVAudioSessionCategoryPlayback error: &error];
+    if (error != nil) {
+        NSLog(@"Not able to set audio session for playback: %@", error);
+    }
+    AVAudioPlayer* newPlayer = [[AVAudioPlayer alloc] initWithData: audioData error: &error];
+    if (newPlayer == nil) {
+        NSLog(@"Unable to play TTS audio data: %@", error);
+        // Real applications probably shouldn't display an alert.
+        [self showAlertWithTitle: @"An error occurred"
+                         message: @"Unable to play audio."];
+    }
+    [newPlayer play];
+    self.audioPlayer = newPlayer;
+}
+
+
+- (void) stopPlaying
+{
+    AVAudioPlayer* oldPlayer = self.audioPlayer;
+    if (oldPlayer != nil) {
+        [oldPlayer stop];
+        self.audioPlayer = nil;
+    }
+}
+
+
+- (IBAction) listen: (id) sender
+{
+    // Don't let TTS playback interfere with audio capture.
+    [self stopTTS];
+    
+    NSLog(@"Starting speech request");
+    
+    // Start listening via the microphone.
+    ATTSpeechService* speechService = [ATTSpeechService sharedSpeechService];
+    
+    // Add extra arguments for speech recogniton.
+    // The parameter is the name of the current screen within this app.
+    speechService.xArgs =
+    [NSDictionary dictionaryWithObjectsAndKeys:
+     @"main", @"ClientScreen", nil];
+    
+    [speechService startListening];
+}
+
+
+- (void) handleRecognition: (NSString*) recognizedText
+{
+    // Display the recognized text.
+    
+    // Speak a response.
+    NSString* response =
+    [NSString stringWithFormat: NSLocalizedString(@"Answer %@", @"Answer"),
+     recognizedText];
+    [self startTTS: response];
+}
+
+#pragma mark -
+#pragma mark Speech Service Delegate Methods
+
+
+- (void) speechServiceSucceeded: (ATTSpeechService*) speechService
+{
+    NSLog(@"Speech service succeeded");
+    
+    // Extract the needed data from the SpeechService object:
+    // For raw bytes, read speechService.responseData.
+    // For a JSON tree, read speechService.responseDictionary.
+    // For the n-best ASR strings, use speechService.responseStrings.
+    
+    // In this example, use the ASR strings.
+    // There can be 0 strings, 1 empty string, or 1 non-empty string.
+    // Display the recognized text in the interface is it's non-empty,
+    // otherwise have the user try again.
+    NSArray* nbest = speechService.responseStrings;
+    NSString* recognizedText = @"";
+    if (nbest != nil && nbest.count > 0)
+        recognizedText = [nbest objectAtIndex: 0];
+    if (recognizedText.length) { // non-empty?
+        [self handleRecognition: recognizedText];
+    }
+    else {
+        [self showAlertWithTitle: @"Didn't recognize speech"
+                         message: @"Please try again."];
+    }
+}
+
+
+- (void) speechService: (ATTSpeechService*) speechService
+       failedWithError: (NSError*) error
+{
+    if ([error.domain isEqualToString: ATTSpeechServiceErrorDomain]
+        && (error.code == ATTSpeechServiceErrorCodeCanceledByUser)) {
+        NSLog(@"Speech service canceled");
+        // Nothing to do in this case
+        return;
+    }
+    NSLog(@"Speech service had an error: %@", error);
+    
+    [self showAlertWithTitle: @"An error occurred"
+                     message: @"Please try again later."];
+}
+
+#pragma mark -
+#pragma mark OAuth
+
+
+- (void) validateOAuthForService: (ATTSpeechService*) speechService
+{
+    [[SpeechAuth authenticatorForService: SpeechOAuthUrl()
+                                  withId: SpeechOAuthKey()
+                                  secret: SpeechOAuthSecret()
+                                   scope: SpeechOAuthScope()]
+     fetchTo: ^(NSString* token, NSError* error) {
+         if (token) {
+             self.oauthToken = token;
+             speechService.bearerAuthToken = token;
+             [self readyForSpeech];
+         }
+         else {
+             self.oauthToken = nil;
+             [self speechAuthFailed: error];
+         }
+     }];
+}
+
+
+- (void) speechAuthFailed: (NSError*) error
+{
+    NSLog(@"OAuth error: %@", error);
+    [self showAlertWithTitle: @"Speech Unavailable"
+                     message: @"This app was rejected by the speech service.  Contact the developer for an update."];
+}
+
+
+- (void) showAlertWithTitle: (NSString*) title message: (NSString*) message
+{
+    UIAlertView* alert =
+    [[UIAlertView alloc] initWithTitle: title
+                               message: message
+                              delegate: self
+                     cancelButtonTitle: @"OK"
+                     otherButtonTitles: nil];
+    [alert show];
+}
+*/
 
 #pragma mark - boiler plate
 
